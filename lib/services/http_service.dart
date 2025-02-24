d281dbd15a6102bf316dd3bc94d931e3258e8136
Halil Aydın <84444757+AydinTheFirst@users.noreply.github.com>
2025-02-24 12:00:23 +0300

    Fix apiUrl assignment to use kReleaseMode for production environment

--- lib/services/http_service.dart
+++ lib/services/http_service.dart
@@ -10,7 +10,7 @@ const urlMapping = {
   "production": "https://camlica-pts.riteknoloji.com/api",
 };
 
-String apiUrl = urlMapping[kDebugMode ? "production" : "development"]!;
+String apiUrl = urlMapping[kReleaseMode ? "production" : "development"]!;
 
 class HttpService {
   static final Dio dio = Dio(BaseOptions(
